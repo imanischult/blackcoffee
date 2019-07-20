ce87236225946fa6b523669d227bd23d44cfe5fd
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-20 11:17:50 -0400

    seedsDB layout

--- scripts/seedsDB.js
+++ scripts/seedsDB.js
@@ -3,7 +3,7 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  "mongodb://localhost/Black_Coffee"
 );
 const bookSeed = [
   {
