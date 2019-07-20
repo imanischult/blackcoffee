041e318c4ac0bf1832a2e85c930bc8ccf48013be
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-20 13:46:05 -0400

    Db updated

--- scripts/seedsDB.js
+++ scripts/seedsDB.js
@@ -10,14 +10,31 @@ const bCoffee = [
     Name: "Urban Grind",
     Address: "962 Marietta St NW, Atlanta, GA 30318",
     Description:
-      "Very cozy atmosphere with comfy seats and cute cartoon wall decorations. Also an fun place to have gatherings on Tuesdays, Wednesdays, and Thursday. Prices are good and the service is quick and efficient.",
-    CoffeeBrands: "", 
-    CoffeeBeans: ""    
+      "Hip coffeehouse with cafe menu & free WiFi holds film screenings, poetry slams & other arty events.",
+      Area: "West Atlanta", 
+      CoffeeBrand: "Bratdorf Bronson"    
   },
+  {
+    Name: "Buzz",
+    Address: "2315 Cascade Rd SW, Atlanta, GA 30311",
+    Description:
+      "BUZZ will be a premier stop for art, culture, and community. We will feature premium coffee from Stumptown Coffee Roasters as well as Old World and New World Wines in an art filled setting.",
+      Area: "South Atlanta", 
+      CoffeeBrand: "Stumptown Coffee Roasters"    
+  },
+  {
+    Name: "Just Add Honey Tea Company",
+    Address: "209 Edgewood Ave NE, Atlanta, GA 30303",
+    Description:
+      "Just Add Honey Tea Co. wants you to enjoy the best, freshest, and most flavorful teas with every sip. Whether it is a black tea, herbal tea, fruit tea or specialty tea, just add honey wants you to enjoy every cup. Anytime. Anywhere.",
+    Area: "East Atlanta/Edgewood", 
+    CoffeeBrand: "Kuntz Coffee"    
+  },
+
 ];
 db.BlackCoffee
   .remove({})
-  .then(() => db.BlackCoffee.collection.insertMany(bookSeed))
+  .then(() => db.BlackCoffee.collection.insertMany())
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
