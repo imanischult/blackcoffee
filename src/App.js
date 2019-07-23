71083f447897522bb99faf33d6c51decbef049ac
Rabbit <Corbineau@users.noreply.github.com>
2019-07-22 22:12:15 -0400

    moved the searchbar, removed uneeded dependency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
-import Searchbar from "./components/Searchbar/Searchbar";
 import Router from "./Router";
 // import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div>
-      <Searchbar />
       <div className="App">
         <header className="App-header" />
         <Router />
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import IntroInfo from "../IntroInfo/IntroInfo";
+import Searchbar from "./components/Searchbar/Searchbar";
 
 const HomePage = () => {
   return (
     <div>
       <IntroInfo />
+      <Searchbar />
     </div>
   );
 };
