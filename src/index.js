6984f17f52c34c92bd35a6005f2944a5bd807661
Nia Reddix <nreddix24@gmail.com>
2019-07-23 20:57:06 -0400

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render((
     <Router>
         <App />
-<<<<<<< HEAD
-    </Router>
-), document.getElementById('root'));
-=======
     </Router>)
     , document.getElementById('root'));
->>>>>>> f789a2bed2fa3ae2e441b015c0175c3d06a4ac77
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
