37b8f97cb97889785e81b177cf98487fb57ac7a7
Rabbit <Corbineau@users.noreply.github.com>
2019-07-23 21:19:31 -0400

    added basic component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Router from "./Router";
+import Navbar from "./components/Navbar/Navbar";
 // import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div>
       <div className="App">
-        <header className="App-header" />
+        <Navbar />
         <Router />
       </div>
     </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+//including for convienience
+
+export default () => {
+  return (
+    <div className="navbar ">
+      <h1>ATL Black Bean</h1>
+
+      <a href="/">Home</a>
+      <a href="/Review">Add a Review</a>
+    </div>
+  )
+}
