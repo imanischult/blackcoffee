bc3b82828c6c7fd1d78c6b65c53771a765ae8bf4
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-24 22:56:45 -0400

    Thank you Mr. Saget - setting up db connection to app

--- routes/api/coffee.js
+++ routes/api/coffee.js
@@ -1,6 +1,14 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const models = require('../../models');
 
 router.get('/coffee', function(req, res) {
-    console.log(models.coffeeShop)
-})
+    models.coffeeShop.create({
+        name: 'Best cofvefe ever!',
+        address: "123 Main st",
+        description: "Best infuraiting coffee wver"
+    })
+   res.sendStatus(200)
+});
+
+
+module.exports = router;
