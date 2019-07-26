243ef4b89abc67660a5e2d43b73256fb7c5b3faf
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-07-25 21:58:32 -0400

    added root route

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
