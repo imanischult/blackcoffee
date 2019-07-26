17e9bf82e25ed0a82165f249ee5add86f26decf9
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-25 21:23:45 -0400

    error fixing

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Navbar.css";
+import { makeStyles } from "@material-ui/core/styles";
+//including for convienience
+
+export default () => {
+  return (
+    <div className="navbar ">
+      <h1>ATL Black Bean</h1>
+
+      <a href="/">Home</a>
+      <a href="/Review">Add a Review</a>
+    </div>
+  )
+}
