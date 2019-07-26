7ee81ab0a75c6fcfc2a8fe878a81a19739610a63
IDM.air <innanadm8@gmail.com>
2019-07-25 21:00:43 -0400

    added Searchbar and button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Searchbar from "./components/Searchbar/Searchbar";
+import Button from "./components/Button";
+import Router from "./Router";
 import "./App.css";
 
 // import { Button } from "@material-ui/core";
@@ -8,6 +10,8 @@ function App() {
   return (
     <div>
       <Searchbar />
+      <Button />
+
       <div className="App">
         <header className="App-header" />
         <Router />
---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>text</p>
+    </div>
+  );
+};
+
+export default HomePage;
