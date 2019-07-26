3d1033839ed4f0d57cd93429be5c20d9994d9578
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-07-25 22:01:11 -0400

    fixing express error

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3006;
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
