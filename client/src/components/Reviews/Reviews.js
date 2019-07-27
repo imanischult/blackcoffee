911876dbec7b56a4ebe5c7c80f1d0448faa8d651
corbineau <lochinvar@gmail.com>
2019-07-27 13:11:27 -0400

    added basic review content

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 
 class Reviews extends Component {
     state={
+        SelectedShop: "",
+        currentReview: "",
+        currentName: "",
+        reviewResults: []
 
     }
 
-    
+    componentDidMount() {
+
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
 
     render() {
         return (
             <div>
-                
+                <form className="form-group" name="reviewForm">
+                    
+                    <input type="dropdown" name="shop-name">select a coffee shop</input>
+                    <textarea name="reviewBody"></textarea>
+                    <input name="reviewerName">Enter your name here</input>
+                    <button type="submit">Submit</button>
+
+                </form>
+                <div className="results">
+                    No reviews yet.
+                </div>
             </div>
         );
     }
