db50b056cbd9ac078d2a702f8845e0d6dee67bfc
corbineau <lochinvar@gmail.com>
2019-07-27 13:58:58 -0400

    all AC done except for the state changing smoothly

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 class Reviews extends Component {
-    state={
-        SelectedShop: "",
+    state = {
+        selectedShop: "",
         currentReview: "",
         currentName: "",
         reviewResults: []
@@ -10,26 +10,52 @@ class Reviews extends Component {
     }
 
     componentDidMount() {
+        //can we have the selected shop pre-set if you come here from a search-result?
 
     }
 
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
-          [name]: value
+            [name]: value
         });
-      };
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(this.state);
+    };
 
     render() {
         return (
             <div>
                 <form className="form-group" name="reviewForm">
-                    
-                    <input type="dropdown" name="shop-name">select a coffee shop</input>
-                    <textarea name="reviewBody"></textarea>
-                    <input name="reviewerName">Enter your name here</input>
-                    <button type="submit">Submit</button>
-
+                    <label>Choose a Coffee Shop
+                    <input
+                            type="dropdown" name="shop-name"
+                            value={this.state.selectedShop}
+                            onChange={this.handleInputChange} >
+                        </input>
+                    </label>
+                    <br />
+                    <label for="review-body">Write your review here
+                        <textarea name="review-body"
+                            value={this.state.currentReview}
+                            onChange={this.handleInputChange}
+                        >
+                        </textarea>
+                    </label>
+                    <br />
+                    <label for="reviewer-name">Your Name
+                        <input type="text" name="reviewer-name"
+                            value={this.state.currentName}
+                            onChange={this.handleInputChange}
+                        >
+                        </input>
+                    </label>
+                    <button type="submit"
+                        onClick={this.handleFormSubmit}
+                    >Submit</button>
                 </form>
                 <div className="results">
                     No reviews yet.
