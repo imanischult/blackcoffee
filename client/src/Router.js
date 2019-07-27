ea80860104b6e44cb6eef9ebfd047f7bc59ba444
corbineau <lochinvar@gmail.com>
2019-07-27 12:53:34 -0400

    resolving some oddness in the router

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage.js";
-import Reviews from "./components/Reviews/Reviews.js";
+import Reviews from "./components/Reviews/Reviews.js"
 
 class Router extends Component {
     render() {
@@ -15,14 +15,6 @@ class Router extends Component {
           </Switch>
         </div>
       )
-      return (
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/index" component={HomePage} />
-          <Route path="*" component={HomePage} />
-        </Switch>
-      </div>
-    );
     return (
       <Switch>
         <Router />
