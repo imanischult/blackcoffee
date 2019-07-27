33508a77738b43b9ed35c6e63fd505a8fabf1ed5
Nia Reddix <nreddix24@gmail.com>
2019-07-27 09:57:15 -0400

    pulling from development

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 import Router from "./Router";
-import Navbar from "./components/Navbar/Navbar";
+//import Navbar from "./components/Navbar/Navbar";
 // import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div>
-      <Navbar />
       <div className="App">
         <Router />
       </div>
