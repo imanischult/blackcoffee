530b899a20b42e2cae28a867b7dd3849c81de5e3
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-07-29 17:40:21 -0400

    adding some API functionality for homepage

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import IntroInfo from "../IntroInfo/IntroInfo";
 import Searchbar from "../Searchbar/Searchbar";
+import Button from "./Button.js";
 
 const HomePage = () => {
   return (
     <div>
       <IntroInfo />
       <Searchbar />
+      <Button />
     </div>
   );
 };
