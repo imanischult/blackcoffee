cce69a4a5756ebab1abb7b150c1085bfea0b13ea
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-07-30 18:18:16 -0400

    adding Link functionality from a student

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -3,44 +3,69 @@ import IntroInfo from "../IntroInfo/IntroInfo";
 import Searchbar from "../Searchbar/Searchbar";
 import Button from "../Button";
 import API from "../../utils/API";
+const algoliasearch = require("algoliasearch");
+const client = algoliasearch("98X3GEBMRE", "ec6500df8d948e205285966022d1ae6d");
+const index = client.initIndex("coffeeshops");
 
-class Homepage extends Component {
+class HomePage extends React.Component {
   state = {
     coffeeshops: [],
     shopname: "",
     address: "",
     description: "",
-    coffeebrand: ""
+    coffeebrand: "",
+    search: ""
   };
+  loadCoffeeshops = event => {
+    event.preventDefault();
+    index.search(
+      {
+        query: this.state.search
+      },
+      (err, { hits } = {}) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this.setState({ coffeeshops: hits });
+      }
+    );
+    // use algolia instead of searching backend
+    //API.getCoffeeShop()
+    //.then(res =>
+    //this.setState({
+    //coffeeShops: res.data,
+    //shopname: '',
+    //address: '',
+    //description: '',
+    //coffeebrand: ''
+    //})
+    //)
+    //.catch(err => console.log(err));
+  };
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <nav>temp navbar</nav>
+        <IntroInfo />
+        <form onSubmit={this.loadCoffeeshops}>
+          <input
+            name="search"
+            type="text"
+            value={this.state.search}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Search Local Coffeeshops</button>
+        </form>
 
-  componentDidMount() {
-    this.loadCoffeeshops();
+        {this.state.coffeeshops.map(shop => (
+          <div>{shop.name}</div>
+        ))}
+      </div>
+    );
   }
-
-  loadCoffeeshops = () => {
-    API.getCoffeeShop()
-      .then(res =>
-        this.setState({
-          coffeeShops: res.data,
-          shopname: "",
-          address: "",
-          description: "",
-          coffeebrand: ""
-        })
-      )
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = (event = {});
 }
-// const HomePage = () => {
-//   return (
-//     <div>
-//       <IntroInfo />
-//       <Searchbar />
-//       <Button />
-//     </div>
-//   );
-// };
-
 export default HomePage;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 //including for convienience
 import { makeStyles } from "@material-ui/core/styles";
-
 
 export default () => {
   return (
     <div className="navbar ">
       <h1>ATL Black Bean</h1>
 
-      <a href="/">Home</a>
-      <a href="/Reviews">Reviews</a>
+      <Link to="/">Home</Link>
+      <Link to="/Reviews">Reviews</Link>
     </div>
-  )
-}
+  );
+};
