5fc13ee6e39cf3d6c54d5eeade8822418abc72d0
Nia Reddix <nreddix24@gmail.com>
2019-07-30 20:40:19 -0400

    added updated CSS to homepage

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,14 +3,17 @@ import "./Navbar.css";
 //including for convienience
 import { makeStyles } from "@material-ui/core/styles";
 
+var link = document.createElement('link');
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('type', 'text/css');
+link.setAttribute('href', 'fonts.googleapis.com/css?family=Cabin+Sketch');
+document.head.appendChild(link);
 
 export default () => {
   return (
     <div className="navbar ">
-      <h1>ATL Black Bean</h1>
-
-      <a href="/">Home</a>
-      <a href="/Reviews">Reviews</a>
+      <a id="home" href="/">Home</a>
+      <a id="review" href="/Reviews">Reviews</a>
     </div>
   )
 }
