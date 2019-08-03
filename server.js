97348835e7904f569d7c85ce95e9527b6eccff51
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-08-03 12:17:18 -0400

    fixed merge conflicts - app main function works

--- server.js
+++ server.js
@@ -2,25 +2,19 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-<<<<<<< HEAD
-const coffeeRoutes = require("./routes/api/coffee");
+const routes = require("./routes");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/BlackCoffee", { useNewUrlParser: true });
-
-app.use(coffeeRoutes);
-=======
-const routes= require('./routes')
-const mongoose= require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/BlackCoffee', { useNewUrlParser: true })
+mongoose.connect("mongodb://127.0.0.1:27017/BlackCoffee", {
+  useNewUrlParser: true
+});
 //
 let db = mongoose.connection;
 
-db.once('open', () => console.log('connected to the database'));
+db.once("open", () => console.log("connected to the database"));
 
 // checks if connection with the database is successful
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
@@ -28,7 +22,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
->>>>>>> 16201b5d04b35f65a0ec88540883fab05a358824
 
 // Start the API server
 app.listen(PORT, function() {
