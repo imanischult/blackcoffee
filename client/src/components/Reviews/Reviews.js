46142b1f7499a7568d0af19c4ee834404e43a9bf
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-08-08 16:52:54 -0400

    fixing merge conflicts

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -86,9 +86,10 @@ class Reviews extends Component {
     return (
       <div className="App-header">
         <form className="form-group" name="reviewForm">
-          <label>
+          <label className="CCSInput">
             Choose a Coffee Shop:
             <select
+              className="CCSSelect"
               name="selectedShop"
               value={this.state.selectedShop}
               onChange={this.handleNameSelect}
@@ -101,25 +102,31 @@ class Reviews extends Component {
             </select>
           </label>
           <br />
-          <label>
+          <label className="Review">
             Write your review here
             <textarea
+              className="ReviewTextArea"
               name="reviewbody"
               value={this.state.reviewbody}
               onChange={this.handleInputChange}
             />
           </label>
           <br />
-          <label>
+          <label className="YourNameLabel">
             Your Name
             <input
+              className="YourNameInput"
               type="text"
               name="reviewer"
               value={this.state.reviewer}
               onChange={this.handleInputChange}
             />
           </label>
-          <button type="submit" onClick={this.handleFormSubmit}>
+          <button
+            className="SubmitButn"
+            type="submit"
+            onClick={this.handleFormSubmit}
+          >
             Submit
           </button>
         </form>
@@ -139,3 +146,155 @@ class Reviews extends Component {
 }
 
 export default Reviews;
+
+// import React, { Component } from "react";
+// import API from "../../utils/API";
+// import "./Reviews.css";
+// import ReviewCard from "../ReviewCard/ReviewCard.js";
+// // import { makeStyles } from "@material-ui/core/styles";
+// // import { get } from "https";
+
+// class Reviews extends Component {
+//   state = {
+//     shops: [],
+//     selectedShop: "",
+//     reviewbody: "",
+//     reviewer: "",
+//     reviewResults: [],
+//     selectedShopId: "",
+//     hasReviews: false
+//   };
+//   componentDidMount() {
+//     API.getCoffeeShop()
+//       .then(res => {
+//         // console.log(res);
+//         let shopNames = res.data.map(shop => {
+//           return { value: shop.name, display: shop.name, id: shop._id };
+//         });
+//         this.setState({
+//           shops: [{ value: "", display: "(Select a coffee shop)" }].concat(
+//             shopNames
+//           ),
+//           selectedShop: "",
+//           selectedShopId: ""
+//         });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+
+//   handleInputChange = event => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value
+//     });
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     console.log(this.state);
+//   };
+
+//   handleNameSelect = event => {
+//     const { name, value, key } = event.target;
+//     this.setState(
+//       {
+//         [name]: value,
+//         selectedShopId: key
+//       },
+//       this.getReviews
+//     );
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     //form validations will have to happen here
+//     API.createReview({
+//       coffeeShopId: this.state.selectedShopId,
+//       coffeeShopName: this.state.selectedShop,
+//       reviewer: this.state.reviewer,
+//       review_text: this.state.reviewbody
+//     })
+//       .then(this.getReviews())
+//       .then(
+//         this.setState({
+//           reviewbody: "",
+//           reviewer: ""
+//         })
+//       );
+//   };
+
+//   getReviews = () => {
+//     const shopName = this.state.selectedShop;
+//     API.getShopReviewsByName(shopName).then(res => {
+//       this.setState({
+//         reviewResults: res.data
+//       });
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App-header">
+//         <form className="form-group" name="reviewForm">
+//           <label className="CCSInput">
+//             Choose a Coffee Shop:
+//             <select
+//               className="CCSSelect"
+//               name="selectedShop"
+//               value={this.state.selectedShop}
+//               onChange={this.handleNameSelect}
+//             >
+//               {this.state.shops.map(shop => (
+//                 <option key={shop.id} value={shop.value}>
+//                   {shop.display}
+//                 </option>
+//               ))}
+//             </select>
+//           </label>
+//           <br />
+//           <label className="Review">
+//             Write your review here
+//             <textarea
+//               className="ReviewTextArea"
+//               name="reviewbody"
+//               value={this.state.reviewbody}
+//               onChange={this.handleInputChange}
+//             />
+//           </label>
+//           <br />
+//           <label className="YourNameLabel">
+//             Your Name
+//             <input
+//               className="YourNameInput"
+//               type="text"
+//               name="reviewer"
+//               value={this.state.reviewer}
+//               onChange={this.handleInputChange}
+//             />
+//           </label>
+//           <button
+//             className="SubmitButn"
+//             type="submit"
+//             onClick={this.handleFormSubmit}
+//           >
+//             Submit
+//           </button>
+//         </form>
+//         <div>
+//           {this.state.reviewResults.map(reviewResult => (
+//             <ReviewCard
+//               key={reviewResult._id}
+//               reviewer={reviewResult.reviewer}
+//               date={reviewResult.date}
+//               review_text={reviewResult.review_text}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Reviews;
--- routes/api/index.js
+++ routes/api/index.js
@@ -6,10 +6,6 @@ const reviewRoutes = require("./reviews");
 // Coffee Routes
 router.use("/coffee", coffeeRoutes);
 router.use("/reviews", reviewRoutes);
-<<<<<<< HEAD
-=======
-
->>>>>>> d79d44e63e97aaf9eace4da4a6b7c95c3c3bc5f8
 
 // For anything else, render the html page
 // router.use(function(req, res) {
