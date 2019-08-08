a31fc90eba7158bd21ac9743c4b61bfcb3d2fb64
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-08-08 03:18:02 -0400

    fixing prod err #9

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  res.sendFile(path.join(__dirname + "client/build/index.html"));
 });
 
 app.use(routes);
