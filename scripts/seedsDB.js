d543f06c509ec8a590844e0a84183e73c948db51
IDM.air <innanadm8@gmail.com>
2019-08-08 05:02:57 -0400

    add image

--- scripts/seedsDB.js
+++ scripts/seedsDB.js
@@ -4,7 +4,7 @@ const db = require("../models");
 const algoliasearch = require("algoliasearch");
 require("dotenv").config();
 
-const client = algoliasearch("V63NYRH7LN", process.env.ALGOLIA_KEY);
+const client = algoliasearch("V63NYRH7LN", "9380ecbed812963b73d661779906c9d2");
 const index = client.initIndex("coffeeshops");
 
 async function asyncForEach(array, callback) {
@@ -32,6 +32,8 @@ const coffeeShops = [
     description:
       "BUZZ will be a premier stop for art, culture, and community. We will feature premium coffee from Stumptown Coffee Roasters as well as Old World and New World Wines in an art filled setting.",
     area: "South Atlanta",
+    image:
+      "https://lh3.googleusercontent.com/amltWqR_j7OIIyvyATyGdjHwIfA8fcGcatQbPHkwxlHTC5wf0lDtqtLylkKUQvFr8y22=s113",
     coffeeBrand: "Stumptown Coffee Roasters"
   },
   {
@@ -40,6 +42,8 @@ const coffeeShops = [
     description:
       "Just Add Honey Tea Co. wants you to enjoy the best, freshest, and most flavorful teas with every sip. Whether it is a black tea, herbal tea, fruit tea or specialty tea, just add honey wants you to enjoy every cup. Anytime. Anywhere.",
     area: "East Atlanta/Edgewood",
+    image:
+      "https://lh3.googleusercontent.com/amltWqR_j7OIIyvyATyGdjHwIfA8fcGcatQbPHkwxlHTC5wf0lDtqtLylkKUQvFr8y22=s113",
     coffeeBrand: "Kuntz Coffee"
   }
 ];
