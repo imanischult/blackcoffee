1b6753d8514e69157b77c74ea27fd3cf222d8fbb
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-08-07 21:48:42 -0400

    fixing bug issues

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 const routes = require("./routes");
 const mongoose = require("mongoose");
