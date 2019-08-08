a953d1d39899831d982e2a5bf5962b820eb82946
corbineau <lochinvar@gmail.com>
2019-08-08 03:00:24 -0400

    reviews are now populating; styling is needed.

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,6 +1,10 @@
 const db = require("../models");
 
+// db.Reviews.create({coffeeShopName: "Village Coffee", coffeeShopId: "5d4babd038cbe0558ac4d9a7", reviewer: "Rabbit", review_text: "good sammiches"}).then(something => console.log(something))
 
+// db.Reviews.find({}).then(dbShop => {
+//   console.log(dbShop)
+//   })
 module.exports = {
   findAll: function (req, res) {
     console.log("finding all reviews... ");
@@ -25,10 +29,10 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   createReview: function (req, res) {
-    console.log(`adding review... ${JSON.stringify(req)}`);
+    console.log(`adding review... ${req}`);
     db.Reviews
-      .create(req.body)
-      .then(dbReview => res.json(dbReview))
+      .create(req)
+      .then(dbReview => {res.json(dbReview)})
       .catch(err => res.status(422).json(err));
   }
 };
--- routes/api/coffee.js
+++ routes/api/coffee.js
@@ -1,15 +1,6 @@
 const router = require("express").Router();
 const Controller = require("../../controllers");
 
-// router.get("/coffee", function(req, res) {
-//   models.coffeeShop.create({
-//     name: "Best coffee ever!",
-//     address: "123 Main st",
-//     description: "Best infuraiting coffee ever"
-//   });
-//   res.sendStatus(200);
-// });
-
 router.route("/").get(Controller.Coffee.findAll);
 
 router
