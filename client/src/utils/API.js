16326179dc02948496f93284baf1455e122060df
corbineau <lochinvar@gmail.com>
2019-08-24 14:32:31 -0400

    stashing

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,13 +6,8 @@ export default {
     return axios.get("/api/coffee/");
   },
   // Gets the coffee shop with the given name
-<<<<<<< HEAD
-  getCoffeeShopByName: function(name) {
-    return axios.get("/api/coffee/" + name);
-=======
   getCoffeeShopById: function(shopId) {
     return axios.get("/api/coffee/" + shopId);
->>>>>>> 909e426d3dd64f38cac27e49fdaf6bbee32e7adf
   },
 
   getCoffeeShopByName: function(shopName) {
